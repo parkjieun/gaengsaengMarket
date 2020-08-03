dd817f2f72ef139a73cf6b1428b9d570e3651548
tlagyqls <tlagyqls7@naver.com>
2020-08-03 18:03:24 +0900

    유저 통신 변경 프론트
    Former-commit-id: f166a6a17bfd5a76d9664f2ba97a2436ea415d48

--- frontend/src/components/user/JoinForm.vue
+++ frontend/src/components/user/JoinForm.vue
@@ -11,17 +11,17 @@
                     <v-list>
                         <v-list-item>
                             <v-spacer></v-spacer>
-                                <image-input v-model="avatar" @input="input">
-                                    <div slot="activator">
-                                        <v-avatar size="10rem" v-ripple v-if="!avatar" class="grey lighten-3 mb-3">
-                                            <v-icon size="5rem">mdi-account</v-icon>
-                                        </v-avatar>
-                                        <v-avatar size="10rem" v-ripple v-else class="mb-3">
-                                            <img :src="avatar.imageURL" alt="avatar">
-                                        </v-avatar>
-                                    </div>
-                                </image-input>
-                                <!-- <v-slide-x-transition>
+                            <image-input v-model="avatar" @input="input">
+                                <div slot="activator">
+                                    <v-avatar size="10rem" v-ripple v-if="!avatar" class="grey lighten-3 mb-3">
+                                        <v-icon size="5rem">mdi-account</v-icon>
+                                    </v-avatar>
+                                    <v-avatar size="10rem" v-ripple v-else class="mb-3">
+                                        <img :src="avatar.imageURL" alt="avatar">
+                                    </v-avatar>
+                                </div>
+                            </image-input>
+                            <!-- <v-slide-x-transition>
                                     <div v-if="avatar && saved == false">
                                         <v-btn class="primary" @click="uploadImage" :loading="saving">Save Avatar</v-btn>
                                     </div>
@@ -29,10 +29,10 @@
                             <v-spacer></v-spacer>
                         </v-list-item>
                         <v-list-item>
-                            <v-text-field  v-model="nickName" :rules="rules" label="닉네임" required></v-text-field>
+                            <v-text-field v-model="nickName" :rules="rules" label="닉네임" required></v-text-field>
                         </v-list-item>
                         <v-list-item>
-                            <v-text-field  :value="address" label="상세 주소" readonly  @click="addressSearch"></v-text-field>
+                            <v-text-field :value="address" label="상세 주소" readonly @click="addressSearch"></v-text-field>
                         </v-list-item>
                         <v-list-item>
                             <v-textarea v-model="introduce" label="상점 소개"></v-textarea>
@@ -60,18 +60,18 @@ import http from "@/util/http-common"
 import ImageInput from '@/components/form/ImageInput.vue'
 export default {
 
-    props: ['dialog2'],
+    props: ['dialog2', 'socialId'],
     data() {
         return {
             avatar: null,
             saving: false,
             saved: false,
-            nickName:"",
-            address:"",
-            introduce:"",
-            img:null,
-            rules:[
-                value=>(value&&value.length<=10)||"10자 이내로 입력해주세요"
+            nickName: "",
+            address: "",
+            introduce: "",
+            img: null,
+            rules: [
+                value => (value && value.length <= 10) || "10자 이내로 입력해주세요"
             ]
         }
     },
@@ -92,36 +92,44 @@ export default {
             this.saving = false
             this.saved = true
         },
-        addressSearch(){
+        addressSearch() {
             var address2;
             var changeAddress = this.changeAddress
             new daum.Postcode({
-                oncomplete: function(data) {
+                oncomplete: function (data) {
                     address2 = data.roadAddress
                     changeAddress(data.roadAddress)
                 }
             }).open();
-            
+
         },
-        changeAddress(value){
-            this.address=value
+        changeAddress(value) {
+            this.address = value
         },
-        input(file){
+        input(file) {
             this.img = file.formData.entries().next().value[1]
         },
-        submit(){
+        submit() {
             const frm = new FormData();
-            frm.append("nickName",this.nickName)
-            frm.append("img",this.img)
-            frm.append("introduce",this.introduce)
-            frm.append("address",this.address)
-            http.post("/api/user",frm,{headers:{Authorization:this.$store.state.authorization,'Content-Type':'multipart/form-data'}})
-            .then(res=>{
-                console.log(res)
-            })
+            frm.append("nickName", this.nickName)
+            if (this.img !== null) {
+                frm.append("img", this.img)
+            }
+            frm.append("socialId",this.socialId)
+            frm.append("introduce", this.introduce)
+            frm.append("address", this.address)
+            http.post("/api/user", frm, {
+                    headers: {
+                        Authorization: this.$store.state.authorization,
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                .then(res => {
+                    console.log(res)
+                })
             this.closeForm()
         },
-        closeForm(){
+        closeForm() {
             this.$emit("closeForm")
         }
     },
