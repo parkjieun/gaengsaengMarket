3e944569ce08051d1a55336aa682c85c3bed80b7
tlagyqls <tlagyqls7@naver.com>
2020-08-03 18:01:51 +0900

    S03P13A504-12 S03P13A504-11 유저 테이블 변경 및 통신 방식에 useriㅇ를 실어 보낸다.
    Former-commit-id: 8e64935fc2bb4a5c61a34ddb41be918f1cae9733

--- backend/account/src/main/java/com/ssafy/user/AccountApplication.java
+++ backend/account/src/main/java/com/ssafy/user/AccountApplication.java
@@ -2,6 +2,9 @@ package com.ssafy.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
 public class AccountApplication {
