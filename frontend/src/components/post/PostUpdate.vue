c96e57244621c262953355e2523264cfa9bda55e
jinseulgi <jinpig2127@naver.com>
2020-08-06 21:00:18 +0900

    자잘한 수정5..
    Former-commit-id: 21766aae86a7719c668f0addf9e891ba84aea8e1

--- frontend/src/components/post/PostUpdate.vue
+++ frontend/src/components/post/PostUpdate.vue
@@ -262,7 +262,7 @@ export default {
         this.toggle_exclusive.push(1);
       }
       let imgs = data.files.split(",");
-      for (let img of imgs) {
+      for (let img of imgs) {http://i3a504.p.ssafy.io:8000/api/static/image
         this.onServerThumnailImgsUrl.push("http://i3a504.p.ssafy.io:8000/api/static/image/" + img);
       }
       this.price = data.price;
@@ -274,7 +274,8 @@ export default {
         this.items.push({ text: splitTags[i], color: this.colors[i] });
       }
 
-      let sum = data.deal_weak
+      let sum = 0;
+      sum = data.deal_weak;
       //요일
       for (let i in sum.toString(2)) {
         if (sum.toString(2)[i] == 1) {
