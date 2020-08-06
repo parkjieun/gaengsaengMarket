4ca1391e59fa64318759e60dec965b71809c4034
tlagyqls <tlagyqls7@naver.com>
2020-08-07 00:37:37 +0900

    채팅 조금 수정
    Former-commit-id: 8ba56e5dc879db5ebef1ce913228055c9ad89158

--- frontend/src/util/http-chat.js
+++ frontend/src/util/http-chat.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:8001",
-//   baseURL: "http://i3a504.p.ssafy.io:8001",
+    // baseURL: "http://localhost:8001",
+  baseURL: "http://i3a504.p.ssafy.io:8001",
   headers: { 
   }
 })
