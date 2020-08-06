9e48421808235ae81c1df042b508221a512666c1
오지수 <wltn0213@gmail.com>
2020-08-06 17:14:29 +0900

    S03P13A504-38 Scroll to Top Button
    Former-commit-id: 7e3475c09c4fd57e425f938708fd18b27c45184e

--- frontend/src/components/post/PostList.vue
+++ frontend/src/components/post/PostList.vue
@@ -1,15 +1,24 @@
 <template>
     <v-row>
         <template v-for="i in len">
-            <v-col :key="i" cols="12" md="4" lg="3" xl="2" align="center" >
+            <v-col :key="i" cols="12" sm="6" md="4" lg="3" xl="2" align="center" >
                 <!-- <router-link :to="'/post/detail?post_id=' + posts[i-1].post_id"> -->
                 <div @click="goDetail(i)">
                 <post-item :post="posts[i-1]" />
                 <!-- </router-link>   -->
-                </div>
-                
+                </div>  
             </v-col>
         </template>
+        <v-btn
+            id="scrollButton"
+            fab
+            small
+            color="#a6e3e9"
+            retain-focus-on-click
+            @click="scrollToTop"
+        >
+            <v-icon color="#3f696e">mdi-chevron-up</v-icon>
+        </v-btn>
     </v-row>  
 </template>
 
@@ -38,12 +47,20 @@ export default {
         goDetail(i){
             console.log(i)
             this.$router.push(`/post/detail?post_id=${this.posts[i-1].post_id}`)
-        }
+        },
+        scrollToTop() {
+            window.scroll({top: 0, left: 0, behavior: 'smooth'})
+        },
     },
 }
 </script>
 
-<style>
-
+<style scoped>
+#scrollButton {
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+}
 
 </style>
