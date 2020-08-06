c04e1e01e06d1d0c53a3b493116db8542839727c
tlagyqls <tlagyqls7@naver.com>
2020-08-06 22:55:23 +0900

    S03P13A504-34 S03P13A504-35 S03P13A504-36 채팅 기능 v1.0
    Former-commit-id: f32084a1fbe458751c37bfc269edf70ee27ab015

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,9 @@
 <template>
-  <v-app id="app">
-    <!-- <div>
-      <CategoryMenu :category="category" @category="openCategory"/>
-    </div>
-    <div id="navbar"> -->
-      <NavBar v-if="!isChat" @openForm="openForm" />
-    <!-- </div> -->
-    
-    <!-- Sizes your content based upon application components -->
-    <!-- <v-main>
-
-      <v-container fluid> -->
-
-        <!-- If using vue-router -->
-        <router-view ></router-view>
-      <!-- </v-container>
-    </v-main> -->
-
-    <!-- <v-footer app>
-
-    </v-footer> -->
-    <login-form  :dialog="dialog" @closeForm="dialog=false"/>
-  </v-app>
+<v-app>
+  <login-form :dialog="dialog" @closeForm="dialog=false" />
+    <NavBar v-if="!isChat" @openForm="openForm" />
+    <router-view></router-view>
+</v-app>
 </template>
 
 <script>
@@ -29,47 +11,47 @@ import NavBar from '@/components/frame/NavBar.vue'
 import CategoryMenu from '@/components/frame/CategoryMenu.vue'
 import LoginForm from "@/components/user/LoginForm.vue"
 export default {
-  components: {
-    NavBar,
-    CategoryMenu,
-    LoginForm
-  },
-  
-  data() {
-    return {
-      dialog:false,
-      category: false
-      
-    }
-  },
-  created(){
-    console.log("qweqwe")
-  },
-  computed:{
-    isChat(){
-      var url = window.location.href
-      console.log(url)
-      return url.includes("chat")
-    }
-  },
-  methods: {
-    openCategory(value) {
-      console.log(value)
-      this.category = value
+    components: {
+        NavBar,
+        CategoryMenu,
+        LoginForm
     },
-    openForm(){
-      this.dialog=true;
+
+    data() {
+        return {
+            dialog: false,
+            category: false
+
+        }
     },
-    closeForm(){
-      this.dialog=false;
-    }
-  },
-  
+    created() {
+        console.log("qweqwe")
+    },
+    computed: {
+        isChat() {
+            var url = window.location.href
+            console.log(url)
+            return url.includes("chat")
+        }
+    },
+    methods: {
+        openCategory(value) {
+            console.log(value)
+            this.category = value
+        },
+        openForm() {
+            this.dialog = true;
+        },
+        closeForm() {
+            this.dialog = false;
+        }
+    },
+
 }
 </script>
 
 <style>
-textarea{
-  resize:none;
+textarea {
+    resize: none;
 }
 </style>
--- frontend/src/components/chat/Chat2.vue
+++ frontend/src/components/chat/Chat2.vue
@@ -1,98 +1,265 @@
 <template>
-    <div class="container" id="app" v-cloak>
-        <div>
-            <h2>{{room.name}}</h2>
+<v-app>
+
+    <section id="myd" class="chatbox">
+		<section class="chat-window">
+			<article v-for="message in messages" :key="message.messageId" :class="message.userId==myProfile.userId?'msg-self':'msg-remote'" class="msg-container" id="msg-0" >
+				<div class="msg-box">
+					<div class="flr">
+						<div class="messages">
+							<p class="msg" id="msg-1">
+								{{message.content}}
+							</p>
+						</div>
+						<span class="timestamp"><span class="username">Name</span>&bull;<span class="posttime">Now</span></span>
+					</div>
+					<img class="user-img" id="user-0" :src="myProfile.profileImg?'http://i3a504.p.ssafy.io/:8003'+myProfile.profileImg:'https://static.thenounproject.com/png/3069450-200.png'" />
+				</div>
+			</article>
+            
+		</section>
+		<div class="chat-input" >
+			<input type="text" v-model="text" @keydown.enter="sendMessage" />
+			<button @click="sendMessage">
+                <v-icon style="color:black;">
+                    mdi-send
+                </v-icon>
+            </button>
+		</div>
+        <div style="height:50px;">
         </div>
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <label class="input-group-text">내용</label>
-            </div>
-            <input type="text" class="form-control" v-model="text" v-on:keypress.enter="sendMessage">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button" @click="sendMessage">보내기</button>
-            </div>
-        </div>
-        <ul class="list-group">
-            <li class="list-group-item" v-for="message in contents" :key="message.message_id">
-                {{message.user_id}} - {{message.content}}
-            </li>
-        </ul>
-        <div></div>
-    </div>
+	</section>
+</v-app>
 </template>
 
 <script>
 import httpChat from "@/util/http-chat"
 import Stomp from 'webstomp-client'
 import SockJS from 'sockjs-client'
-
+import {mapState} from "vuex"
 export default {
-    
+
     data() {
         return {
-            socket:null,
-            stomp:null,
+            socket: null,
+            stomp: null,
+            userId: '',
             roomId: '',
             room: {},
             userId: '',
             content: '',
-            contents: [],
-            text:""
+            messages: [],
+            text: ""
         }
     },
     created() {
         this.roomId = this.$route.params.roomId
         this.userId = this.$store.state.myProfile.userId
-        this.socket = new SockJS("http://localhost:8001/ws-stomp");
+        this.socket = new SockJS("http://i3a504.p.ssafy.io:8001/ws-stomp");
         this.stomp = Stomp.over(this.socket);
         this.findRoom();
         this.connect()
     },
-
+    computed:{
+        ...mapState(['myProfile'])
+    },
     methods: {
         findRoom: function () {
-            httpChat.get('/api/chat/' + this.roomId,{headers:{Authorization: this.$store.state.authorization}}).then(response => {
-                this.contents = response.data;
+            httpChat.get('/api/chat/' + this.roomId, {
+                headers: {
+                    Authorization: this.$store.state.authorization
+                }
+            }).then(response => {
+                this.messages = response.data;
             });
         },
         sendMessage: function () {
-
-            this.stomp.send("/pub/chat/message/"+this.roomId,  JSON.stringify({
-
+            this.stomp.send("/pub/chat/message/" + this.roomId, JSON.stringify({
                 userId: this.userId,
                 content: this.text
-            }),{});
-              
+            }), {});
+
             this.text = '';
+            window.scrollTo(0,document.body.scrollHeight)
         },
         recvMessage: function (recv) {
-            console.log(recv)
-            this.contents.unshift({
+            this.messages.push({
                 "userId": recv.userId,
-                "content": recv.content
+                "content": recv.content,
+                "createDate": recv.createDate
             })
+            window.scrollTo(0,document.body.scrollHeight)
         },
-            connect() {
-        var roomId = this.roomId
-        var userId = this.userId
-        var recvMessage = this.recvMessage
-        var stomp = this.stomp
-        console.log(this.stomp)
-        this.stomp.connect({}, function (frame) {
-            console.log(frame)
-            stomp.subscribe("/sub/chat/room/" + roomId, res=> {
-                var recv = JSON.parse(res.body);
-                console.log(recv)
-                recvMessage(recv);
-            });
+        connect() {
+            var roomId = this.roomId
+            var userId = this.userId
+            var recvMessage = this.recvMessage
+            var stomp = this.stomp
 
-        })
-        console.log(this.stomp)
-    },
+            this.stomp.connect({}, function (frame) {
+                stomp.subscribe("/sub/chat/room/" + roomId, res => {
+                    var recv = JSON.parse(res.body);
+                    recvMessage(recv);
+                });
+
+            })
+
+        },
+
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+html,
+body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background: #1c1e22;
+}
+body {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+    flex-direction: column;
+}
+::-webkit-scrollbar {
+	width: 4px;
+}
+::-webkit-scrollbar-thumb {
+	background-color: #fff;
+	border-radius: 2px;
+}
+.chatbox {
+    width: 100%;
+    height: 100%;
+    max-height: 100%;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    box-shadow: 0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28);
+}
+.chat-window {
+    padding-top:5px;
+    flex: auto;
+    max-height: calc(100% - 60px);
+    background: rgba(92, 92, 92, 0.377);
+    
+}
+.chat-input {
+    flex: 0 0 auto;
+    height: 60px;
+    background: rgb(230,230,230);
+    border-top: 1px solid #2671ff;
+    box-shadow: 0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28);
+    position: fixed;
+    bottom:0px;
+    width:100%;
+}
+.chat-input input {
+    height: 59px;
+    line-height: 60px;
+    outline: 0 none;
+    border: none;
+    width: calc(100% - 60px);
+    color: black;
+    text-indent: 10px;
+    font-size: 12pt;
+    padding: 0;
+    background: rgb(230,230,230);
+}
+.chat-input button {
+    float: right;
+    outline: 0 none;
+    border: none;
+    height: 40px;
+    width: 40px;
+    color:blue;
+    border-radius: 50%;
+    padding: 2px 0 0 0;
+    margin: 10px;
+    transition: all 0.15s ease-in-out;
+}
+.chat-input input[good] + button {
+    box-shadow: 0 0 2px rgba(0,0,0,.12),0 2px 4px rgba(0,0,0,.24);
+    background: #2671ff;
+}
+.chat-input input[good] + button:hover {
+    box-shadow: 0 8px 17px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+}
+.chat-input input[good] + button path {
+    fill: white;
+}
+.msg-container {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    margin: 0 0 10px 0;
+    padding: 0;
+}
+.msg-box {
+    display: flex;
+    background: #5b5e6c;
+    padding: 10px 10px 0 10px;
+    border-radius: 0 6px 6px 0;
+    max-width: 80%;
+    width: auto;
+    float: left;
+    box-shadow: 0 0 2px rgba(0,0,0,.12),0 2px 4px rgba(0,0,0,.24);
+}
+.user-img {
+    display: inline-block;
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+    background: #2671ff;
+    margin: 0 10px 10px 0;
+}
+.flr {
+    flex: 1 0 auto;
+    display: flex;
+    flex-direction: column;
+    width: calc(100% - 50px);
+}
+.messages {
+    flex: 1 0 auto;
+}
+.msg {
+    display: inline-block;
+    font-size: 11pt;
+    line-height: 13pt;
+    color: rgba(255,255,255,.7);
+    margin: 0 0 4px 0;
+}
+.msg:first-of-type {
+    margin-top: 8px;
+}
+.timestamp {
+    color: rgba(0,0,0,.38);
+    font-size: 8pt;
+    margin-bottom: 10px;
+}
+.username {
+    margin-right: 3px;
+}
+.posttime {
+    margin-left: 3px;
+}
+.msg-self .msg-box {
+    border-radius: 6px 0 0 6px;
+    background: #2671ff;
+    float: right;
+}
+.msg-self .user-img {
+    margin: 0 0 10px 10px;
+}
+.msg-self .msg {
+    text-align: right;
+}
+.msg-self .timestamp {
+    text-align: right;
+}
 </style>
