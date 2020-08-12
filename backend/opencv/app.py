c3ab875225623939a60c7853ec1c9a01c0bb110e
tlagyqls <tlagyqls7@naver.com>
2020-08-12 14:24:35 +0900

    S03P13A504-59 형태소 분석기 flask에 추가 완료
    Former-commit-id: ff2c33dd8c81d0dfb0895f58887f73fc4e6e4b80

--- backend/opencv/app.py
+++ backend/opencv/app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import extractTags
+from title_analyzer import TitleAnalyzer
 
 app = Flask(__name__)
 CORS(app)
+title_analyzer = TitleAnalyzer()
 
 @app.route('/')
 def home():
@@ -23,6 +25,18 @@ def upload_file():
 
       print("진짜 최종 태그들...",resultTags)
       return ','.join(resultTags)
+
+@app.route('/category', methods = ['GET'])
+def category_by_title():
+    return str(title_analyzer.get_category(request.args.get('title')))
+    
+@app.route('/hashtag', methods = ['GET'])
+def keyword_by_title():
+    return str(title_analyzer.get_nouns(request.args.get('title')))
+
+@app.route('/price', methods=['GET'])
+def price_by_title():
+    return str(title_analyzer.get_price(request.args.get('title')))
 
 if __name__ == '__main__':
     app.debug = True
