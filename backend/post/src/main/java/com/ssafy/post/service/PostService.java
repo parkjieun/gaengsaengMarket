ffbaf8423ca745259345647d6c8785949799ed8b
박지은 <wong_303@naver.com>
2020-08-12 12:48:48 +0900

    [S03P13A504-58] 해시태그 및 검색어 분리
    Former-commit-id: fbca47511d82d4e6ed9e97647e5a6e29594a7a19

--- backend/post/src/main/java/com/ssafy/post/service/PostService.java
+++ backend/post/src/main/java/com/ssafy/post/service/PostService.java
@@ -14,7 +14,7 @@ public interface PostService {
 
 	public int deletePost(int post_id)  throws Exception;
 
-	public List<Post> selectAllPost(String query, String user_id, boolean like, int type, int sno)  throws Exception;
+	public List<Post> selectAllPost(String query, String user_id, boolean like, int type, int sno, List<String> tags)  throws Exception;
 
 	public Post detailPost(int post_id,String user_id)  throws Exception;
 
