9269e1642625c93e9227db55aba9b70297cbcccb
unknown <wltn0213@gmail.com>
2020-08-12 18:20:21 +0900

    main page frontend
    Former-commit-id: b53db30b225158ee93d9593c751b30d4efe57682

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -12,17 +12,30 @@
                 </v-carousel>
             </v-col>
         </v-row>
-        <div id="postList" style="width:80%; margin-left:auto; margin-right:auto;"> 
-            <post-list :posts="posts"/>
+        <div class="postList" style="width:80%; margin-left:auto; margin-right:auto;">
+            <h3>최근 등록 상품</h3>
+            <hr>
+            <PostListCarousel :posts="recentPosts"/>
         </div>
-        <infinite-loading @infinite="infiniteHandler" spinner="waveDots"></infinite-loading>
+        <div style="width: 80%; margin-left:auto; margin-right:auto;">
+            <h3>카테고리별 상품</h3>
+            <v-tabs style="width: 100%;" grow>
+                <v-tab v-for="name in categorytab" :key="name" @click="goToCategory">{{name}}</v-tab>
+            </v-tabs>
+        </div>
+        <div class="postList" style="width:80%; margin-left:auto; margin-right:auto;" v-for="post in categoryPosts" :key="post[0]">
+            <div class="categoryTitles" id="categoryTitle" style="align-items:center;"><v-icon small >mdi-alpha-c-circle</v-icon><span style="margin: 5px 5px 5px 5px;"> {{ post[0] }} </span><v-btn text small style="margin-left:10px; color: gray;">전체보기</v-btn></div> 
+            <PostList :posts="post[1]"/>
+        </div>
+
     </v-container>
 </v-app>
 </template>
-
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script>
 import banner from "@/assets/banners/banner.png"
 import CategoryBar from "@/components/frame/CategoryBar.vue"
+import PostListCarousel from "@/components/post/PostListCarousel.vue"
 import PostList from "@/components/post/PostList.vue"
 import CategoryMenu from "@/components/frame/CategoryMenu.vue"
 import CategoryTabs from "@/components/frame/CategoryTabs.vue"
@@ -34,55 +47,81 @@ export default {
             search: "",
             select:[],
             items: [],
-            posts: [],
-            start: 0,
-         
+            recentPosts: [],
+            categoryPosts: [],
+            categorytab: []
+            // #app > div > div > div:nth-child(4) > div.categoryTitles
+            // #app > div > div > div:nth-child(5) > div.categoryTitles
         }
     },
     components: {
         PostList,
+        PostListCarousel,
         CategoryBar,
         CategoryMenu,
         CategoryTabs,
         InfiniteLoading,
     },
+    computed: {
+    },
     modules: {
         banner
     },
     methods: {
-
-
         goMyProfile() {
             this.$router.push({ name: 'MyProfile' })
         },
         goPostCreate() {
             this.$router.push( { name: 'post-create' })
         },
-        infiniteHandler($state) {
-            console.log("스크롤")
-
-            httpPost.get("/api/post?sno=" + this.start)
-            .then(res => {
-                console.log(res)
-                if (res.data.length) {
-                    this.start += 12;
-                    this.posts.push(...res.data);
-                    $state.loaded();
-                } 
-                else {
-                    $state.complete();
-                }
-            });
-        } 
+        setCategoryId() {
+            var list = document.getElementsByClassName("categoryTitles");
+            console.log(list)
+            for (var i = 0; i < list.length; i++) {
+                console.log(list.item(i))
+            }
+        },
+        goToCategory(i) {
+            $('html, body').animate({scrollTop: $("#categoryTitle[i]").offset().top}, 100);
+            
+        },
 
     },
+    created() {
+        httpPost.get('/api/post?sno=0')
+        .then(res => {
+            this.recentPosts = res.data
+        })
+        httpPost.get('/api/post/category/category_big')
+        .then(res => {
+            for (var i = 0; i < res.data.length; i ++) {
+                const cate_name = res.data[i].name
+                httpPost.get(`/api/post/category/big/${res.data[i].cate_big_id}?sno=0`)
+                .then(posts => {
+                    if (posts.data.length) {
+                        this.categoryPosts.push([cate_name, posts.data.slice(0, 8)])
+                        this.categorytab.push(cate_name)
+                    }         
+                })
+            }
+        })
+        .then(
+            this.setCategoryId()
+        )
+        
+    },
     mounted() {
+        
    
     }
 }
 </script>
 
 <style scoped>
+h3 {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
 .tag-input span.chip {
   background-color: #1976d2;
   color: #fff;
