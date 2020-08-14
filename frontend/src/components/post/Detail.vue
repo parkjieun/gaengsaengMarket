59878f609f914ded5474baf6e84dea7e76ff8aa2
박지은 <wong_303@naver.com>
2020-08-14 10:21:57 +0900

    [S03P13A504-49] post detail 지도 추가
    Former-commit-id: 7c26d330bb25f818b11234818ca005031e14787a

--- frontend/src/components/post/Detail.vue
+++ frontend/src/components/post/Detail.vue
@@ -25,19 +25,19 @@
 
           <v-col cols="7">
             <div>
-              <div style="font-size:24px; line-height:1.4; font-weight:600; margin-bottom:25px; display:flex">
+              <div style="font-size:40px; line-height:1.4; font-weight:600; margin-bottom:25px; display:flex">
                 {{item.title }} 
               </div>
 
-              <div style="font-size:40px; line-height:1.4; font-weight:550; margin-bottom:25px; text-align:left ">
+              <div style="font-size:30px; line-height:1.4; font-weight:550; margin-bottom:25px; text-align:left ">
                 {{item.price | currency}} <span style="font-size:28px; font-weight:400; margin-left:5px; ">원</span>
               </div>
 
               <v-divider></v-divider>
 
-              <div style="color:rgb(204,204,204); font-size:16px; margin:15px 0 25px 0; text-align:left  ">
+              <div style=" font-size:16px; margin:15px 0 25px 0; text-align:left  ">
                   <img style="width:16px; height:16px; margin-right:5px" :src="require(`@/assets/post/like_click.png`)"> {{item.like_cnt }}
-                  <span style="margin:0 10px 0 10px"> | </span> 
+                  <span style="margin:0 10px 0 10px;color:rgb(204,204,204);"> | </span> 
                   <img style="width:16px; height:16px; margin-right:5px" :src="require(`@/assets/post/dateTime.png`)">
                   <span v-if="item.update_date != null"> {{$moment(item.update_date).format('YYYY-MM-DD')}}</span>
                   <span v-else> {{$moment(item.create_date).format('YYYY-MM-DD')}} </span>
@@ -69,17 +69,17 @@
               </div>
 
               <div style="font-weight: 600px;font-size:13px; margin-right:10px;height:56px; color:#555555;   display: flex;">
-                  <v-col style="border-right: 1.5px solid #f2f3f6; text-align:center"  @click="goPostLike">
-                    <div v-if="item.likeFlag==1"><img calss="txt_btn" :src="require(`@/assets/post/fullheart.png`)"></div>
-                    <div v-else><img calss="txt_btn" :src="require(`@/assets/post/emptyheart.png`)"></div>
+                  <v-col class="hover_big" style="border-right: 1.5px solid #f2f3f6; text-align:center" >
+                    <div  @click="goPostLike" v-if="item.likeFlag==1"><img calss="txt_btn" :src="require(`@/assets/post/fullheart.png`)"></div>
+                    <div  @click="goPostLike" v-else><img calss="txt_btn" :src="require(`@/assets/post/emptyheart.png`)"></div>
                       찜하기
                   </v-col>
-                  <v-col style="border-right: 1.5px solid #f2f3f6; text-align:center">
+                  <v-col class="hover_big" style="border-right: 1.5px solid #f2f3f6; text-align:center">
                     <div @click="createRoom"><img calss="txt_btn" :src="require(`@/assets/post/chat.png`)"></div>
                     연락하기
                   </v-col>
-                  <v-col style="text-align:center"  @click="openDialog">
-                    <div><img calss="txt_btn" :src="require(`@/assets/post/card.png`)"></div>
+                  <v-col class="hover_big" style="text-align:center" >
+                    <div  @click="openDialog"><img calss="txt_btn" :src="require(`@/assets/post/card.png`)"></div>
                     바로구매
                   </v-col>
               </div>
@@ -89,7 +89,7 @@
 
        <v-row>
          <v-col cols="10" >
-            <v-tabs background-color="#fafafa"  :grow="true">
+            <v-tabs background-color="#fafafa"  :grow="true" color="#2D3753" >
               <v-tab ripple>
                 <span  style="font-weight:600;"> 상품정보 </span>
               </v-tab>
@@ -99,7 +99,7 @@
 
               <v-tab-item>
                 <v-card flat>
-                  <v-card-text style=" text-align:left; color:#212121">
+                  <v-card-text style=" text-align:left; color:#212121;font-size: 1rem!important">
                     <v-row >
                       <div v-html="item.contents"></div>
                     </v-row>
@@ -134,7 +134,7 @@
 
         <v-row v-if="myProfile != null" >  
           <v-col cols="12" style="text-align:right"  v-show="item.user_id ==  myProfile.userId" >    
-              <v-btn class="ma-2" tile outlined color="success" @click="goPostUpdate">
+              <v-btn class="ma-2" tile outlined color="success" @click="goPostUpdate" style="background:#2d3753 !important;border-color:#2d3753 !iportant;height:36px;color:#fff !important">
               <v-icon left>mdi-pencil</v-icon> 수정하기
             </v-btn>
           </v-col>
@@ -332,5 +332,25 @@ position: relative;
 .soldOut{
   position:absolute; 
   opacity: 0.8;
+}
+
+.hover_big  img {
+    -webkit-transform:scale(1);
+    -moz-transform:scale(1);
+    -ms-transform:scale(1); 
+    -o-transform:scale(1);  
+    transform:scale(1);
+    -webkit-transition:.3s;
+    -moz-transition:.3s;
+    -ms-transition:.3s;
+    -o-transition:.3s;
+    transition:.3s;
+}
+.hover_big:hover img {
+    -webkit-transform:scale(1.2);
+    -moz-transform:scale(1.2);
+    -ms-transform:scale(1.2);   
+    -o-transform:scale(1.2);
+    transform:scale(1.2);
 }
 </style>
