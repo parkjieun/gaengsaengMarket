460a5e9f366539e3422ded93f5064173475985c8
tlagyqls <tlagyqls7@naver.com>
2020-08-16 01:52:04 +0900

    S03P13A504-68 채팅 구독 버그 수정
    Former-commit-id: 65daaa4b74916f5df250a7da6d804572ee5feb72

--- frontend/src/components/chat/ChatRoom.vue
+++ frontend/src/components/chat/ChatRoom.vue
@@ -1,78 +1,94 @@
 <template>
-    <v-card
-    width="300"
-    class="mx-auto"
-    id="chatRoom"
-    > 
-        <v-toolbar
-        color="#a6e3e9"
-        flat
-        dense
-        >
-            <v-toolbar-title color ="#3f696e" style="font-size:0.9rem" id="chatTitle">
-                <div>
-                    <v-avatar size="30"><v-img :src="patnerImg"/></v-avatar><span class="mx-3">{{patner.nickName}}</span>
-                </div>
-                <v-icon small @click="closeRoom">mdi-close</v-icon>
-                    
-            </v-toolbar-title>
-        </v-toolbar>
+<v-card width="300" class="mx-auto" id="chatRoom">
+    <v-toolbar color="#a6e3e9" flat dense>
+        <v-toolbar-title color="#3f696e" style="font-size:0.9rem" id="chatTitle">
+            <div>
+                <v-avatar size="30">
+                    <v-img :src="patnerImg" />
+                </v-avatar><span class="mx-3">{{patner.nickName}}</span>
+            </div>
+            <v-icon small @click="closeRoom">mdi-close</v-icon>
 
-        <v-list dense id="chatBody" v-if="messages.length">
-            
-            <v-list-item two-line v-for="item in messages" :key="item.messageId">
-                
-                <div v-if="item.userId != userId" class="row">
-                <v-avatar size="30" style="margin-right: 5px;" class="my-auto"><v-img :src="patnerImg"/></v-avatar>
+        </v-toolbar-title>
+    </v-toolbar>
+
+    <v-list dense id="chatBody" v-if="messages.length">
+
+        <v-list-item two-line v-for="item in messages" :key="item.messageId">
+
+            <div v-if="item.userId != userId" class="row">
+                <v-avatar size="30" style="margin-right: 5px;" class="my-auto">
+                    <v-img :src="patnerImg" />
+                </v-avatar>
                 <v-list-item-content class="py-1">
-                    <v-list-item-title class="receiveMsg my-auto caption" >{{item.content}}</v-list-item-title>
+                    <v-list-item-title class="receiveMsg my-auto caption delivery" v-if="isJsonString(item.content)">
+                        <a data-tooltip-text="배송조회" @click="godeli(item.content)" v-text="deliveryLink(item.content)">
+
+                        </a>
+                    </v-list-item-title>
+                    <v-list-item-title class="receiveMsg my-auto caption" v-else>{{item.content}}</v-list-item-title>
+
                     <v-list-item-subtitle style="width:150px; font-size:0.1rem; margin-left:10px;">{{item.createDate | processingDate}}</v-list-item-subtitle>
                 </v-list-item-content>
-                </div>
-                <div v-else class="row justify-end">
-                    <v-list-item-content class="py-1" style="width:auto; max-width:180px;">
-                        <v-list-item-title class="sendMsg my-auto caption" >{{item.content}}</v-list-item-title>
-                        <v-list-item-subtitle style="font-size:0.1rem; text-align:right; margin-right:10px">{{item.createDate | processingDate}}</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-avatar size="30" style="margin-right: 5px;" class="my-auto"><v-img :src="myProfileImg"/></v-avatar>
-                </div>
-            </v-list-item>
-        </v-list>
-        <v-list dense id="chatBody" v-else>
-            <v-list-item style="background: #ffe6eb; font-size:0.7rem">
-                <v-list-item-title style="color: #805d64;">
+            </div>
+            <div v-else class="row justify-end">
+                <v-list-item-content class="py-1" style="width:auto; max-width:180px;">
+                    <v-list-item-title class="sendMsg my-auto caption delivery" v-if="isJsonString(item.content)">
+                        <a data-tooltip-text="배송조회" @click="godeli(item.content)" v-text="deliveryLink(item.content)">
+
+                        </a>
+                        </v-list-item-title>
+                    <v-list-item-title class="sendMsg my-auto caption" v-else>{{item.content}}</v-list-item-title>
+                    <v-list-item-subtitle style="font-size:0.1rem; text-align:right; margin-right:10px">{{item.createDate | processingDate}}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-avatar size="30" style="margin-right: 5px;" class="my-auto">
+                    <v-img :src="myProfileImg" />
+                </v-avatar>
+            </div>
+        </v-list-item>
+    </v-list>
+    <v-list dense id="chatBody" v-else>
+        <v-list-item style="background: #ffe6eb; font-size:0.7rem">
+            <v-list-item-title style="color: #805d64;">
                 채팅 기록이 없습니다. <br>
                 메세지를 보내 채팅을 시작하세요 :)
-                </v-list-item-title>
-            </v-list-item>
-        </v-list>
-                
-     
-        <v-divider></v-divider>
-        <v-row class="chat-input" style="width:300px; margin:0; height:50px;" >
-            <v-col cols="11">
-                <input type="text" v-model="text" @keydown.enter="sendMessage" style="padding:0; width:100%; height:100%; font-size: 0.9rem;" placeholder="채팅내용을 입력해주세요"/>
-            </v-col>
-            <v-col cols="1" style="padding: 10px 0;">
+            </v-list-item-title>
+        </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+    <v-row class="chat-input" style="width:300px; margin:0; height:50px;">
+        <v-col cols="11">
+            <input type="text" v-model="text" @keydown.enter="sendMessage" style="padding:0; width:100%; height:100%; font-size: 0.9rem;" placeholder="채팅내용을 입력해주세요" />
+        </v-col>
+        <v-col cols="1" style="padding: 10px 0;">
             <button @click="sendMessage" style="width:100% height: 100%;">
                 <v-icon small style="color:#3f696e; margin: 0;">
                     mdi-send
                 </v-icon>
             </button>
-            </v-col>
-        </v-row>
-    </v-card>
-  
+        </v-col>
+    </v-row>
+    <delivery-detail :deliveryModal="deliveryModal" @closeModal="closeModal" />
+</v-card>
 </template>
 
 <script>
 import httpChat from "@/util/http-chat"
 import Stomp from 'webstomp-client'
 import SockJS from 'sockjs-client'
-import {mapState} from "vuex"
+import DeliveryDetail from '@/components/chat/DeliveryDetail.vue'
+import axios from 'axios'
+import {
+    mapState
+} from "vuex"
 const baseURL = "http://i3a504.p.ssafy.io"
+var t_key = "DlCHvPTEJjHmTu3U9IlZhA"
 export default {
     props: ['roomId', 'showRoom', 'messages'],
+    components: {
+        DeliveryDetail,
+    },
     data() {
         return {
             socket: null,
@@ -82,22 +98,15 @@ export default {
             myProfileImg: null,
             userId: '',
             content: '',
-            messages: [],
             text: "",
             scroll: $("#chatBody").prop('scrollHeight'),
+            deliveryModal: false,
         }
     },
     created() {
         this.userId = this.$store.state.myProfile.userId
-        this.myProfileImg = baseURL + "/static/image/account/" + this.$store.state.myProfile.profileImg
-        console.log("ak")
-        this.socket = new SockJS("http://i3a504.p.ssafy.io:8001/ws-stomp");
-        this.stomp = Stomp.over(this.socket);
-        this.connect()
-        
-        
     },
-    updated: function() {
+    updated: function () {
         $("#chatBody").scrollTop($("#chatBody").prop('scrollHeight'))
 
     },
@@ -105,30 +114,27 @@ export default {
         showRoom() {
             if (this.showRoom) {
                 $('#chatRoom').show();
-            }  
-            else {
+            } else {
                 $('#chatRoom').hide();
             }
         },
-        
+
     },
-    computed:{
-        ...mapState(['myProfile','patner']),
-        patnerImg () {
+    computed: {
+        ...mapState(['myProfile', 'patner']),
+        patnerImg() {
             return baseURL + "/static/image/account/" + this.patner.profileImg
         }
     },
     methods: {
         sendMessage: function () {
-            if(this.text!==""){
+            if (this.text !== "") {
                 this.stomp.send("/pub/chat/message/" + this.roomId, JSON.stringify({
                     userId: this.userId,
                     content: this.text
                 }), {});
-
                 this.text = '';
             }
-            
         },
         recvMessage: function (recv) {
             this.messages.push({
@@ -137,22 +143,23 @@ export default {
                 "createDate": recv.createDate
             })
         },
-        connect() {
-            var roomId = this.roomId
-            var userId = this.userId
+        connect(roomId) {
+            this.socket = new SockJS("http://i3a504.p.ssafy.io:8001/ws-stomp");
+            // this.socket = new SockJS("http://localhost:8001/ws-stomp");
+            this.stomp = Stomp.over(this.socket);
             var recvMessage = this.recvMessage
             var stomp = this.stomp
-
+            stomp.debug = () => {};
             this.stomp.connect({}, function (frame) {
                 stomp.subscribe("/sub/chat/room/" + roomId, res => {
                     var recv = JSON.parse(res.body);
                     recvMessage(recv);
                 });
             })
-            .catch(err => {
-                console.log(err, "connet")
-            })
-
+        },
+        destroyed() {
+            if (this.socket)
+                this.socket.close()
         },
         itemHeight(item) {
             var i = document.getSelection(item)
@@ -163,27 +170,53 @@ export default {
             this.showRoom = false
             this.$emit('hideRoom', false)
         },
-    },
-    filters:{
-        processingDate(value){
-            const date = new Date(value)
-            return date.getMonth()+"월 "+date.getDate()+"일  "+date.getHours()+"시 "+date.getMinutes()+"분";
+        deliveryLink(content) {
+            const object = JSON.parse(content)
+            return object.content
+        },
+        godeli(content) {
+            const object = JSON.parse(content)
+            axios.get(`http://info.sweettracker.co.kr/api/v1/trackingInfo?t_invoice=${object.content}&t_code=${object.code.replace("\"","").replace("\"","")}&t_key=${t_key}`)
+                .then(res => {
+                    this.$store.dispatch("setDelivery", res.data)
+                    this.deliveryModal = true
+                })
+        },
+        isJsonString(str) {
+            try {
+                var json = JSON.parse(str);
+                return (typeof json === 'object');
+            } catch (e) {
+                return false;
+            }
+        },
+        closeModal() {
+            this.deliveryModal = false
         }
-    }
+    },
+    filters: {
+        processingDate(value) {
+            const date = new Date(value)
+            return date.getMonth() + "월 " + date.getDate() + "일  " + date.getHours() + "시 " + date.getMinutes() + "분";
+        }
+    },
+
 }
 </script>
 
 <style scoped>
 #chatTitle {
-    width:300px;
+    width: 300px;
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
+
 #chatBody {
     overflow-y: scroll;
     height: 300px;
 }
+
 .receiveMsg {
     background: #ffe6eb;
     padding: 5px 5px 5px 10px;
@@ -191,17 +224,22 @@ export default {
     color: #805d64;
     white-space: pre-line;
     width: auto;
-    max-width:180px;
+    max-width: 180px;
 }
+
 .sendMsg {
-    float:right;
+    float: right;
     background: #defcfc;
     padding: 5px 5px 5px 10px;
     border-radius: 20px;
     color: #517373;
     white-space: pre-line;
     width: auto;
-    max-width:180px;
-    
+    max-width: 180px;
+
+}
+
+.delivery a{
+    color: rebeccapurple;
 }
 </style>
