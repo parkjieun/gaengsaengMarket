452827a7f207f4c1f6e4d15a62d630dfa3812b2d
tlagyqls <tlagyqls7@naver.com>
2020-08-16 23:26:02 +0900

    작은 페이지
    Former-commit-id: 5c0cdab3e3e5e65b5289251eaaea862760bd6a03

---
+++ frontend/src/components/page/SmallPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="out" class="text-center">
+      
+      <div id="in">
+          <img src="@/assets/gifs/hourglass.gif">
+      <h1>모바일 버전 준비중입니다.</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+#out{
+    text-align: center;
+    width:100%;
+    height:100%;
+    padding:auto;
+    vertical-align: middle;
+    display: table;
+}
+#in{
+    display:table-cell;
+    vertical-align: middle;
+    /* display:inline-block; */
+    
+}
+</style>
