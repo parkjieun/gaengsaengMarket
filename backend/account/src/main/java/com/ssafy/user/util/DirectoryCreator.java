20187125d5ec4f9025a92383d6d795c5e0f5f462
tlagyqls <tlagyqls7@naver.com>
2020-08-17 15:44:30 +0900

    small
    Former-commit-id: 87b1fe095b5b4c182d2b4dcca0dba19994c2b625

--- backend/account/src/main/java/com/ssafy/user/util/DirectoryCreator.java
+++ backend/account/src/main/java/com/ssafy/user/util/DirectoryCreator.java
@@ -11,27 +11,26 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DirectoryCreator implements ApplicationRunner{
+public class DirectoryCreator implements ApplicationRunner {
 	@Value("${file.upload.directory}")
 	private String directoryPath;
-	
+
 	@Autowired
 	ServletContext context;
-	
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		String rootPath = context.getRealPath(directoryPath);
 		File folder = new File(rootPath);
 		// 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
 		if (!folder.exists()) {
-			try{
-				folder.mkdirs(); //폴더 생성합니다.
-			    
-		        } 
-		        catch(Exception e){
-			    e.getStackTrace();
-			}     
+			try {
+				folder.mkdirs(); // 폴더 생성합니다.
+
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
 		}
 	}
-	
+
 }
